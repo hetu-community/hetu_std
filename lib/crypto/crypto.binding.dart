9f89e3fb44334ed63a0c9ccb6099d3d9f4631462
Kingkor Roy Tirtho <krtirtho@gmail.com>
2025-05-23 19:07:23 +0600

    fix: crypto properly cast List into List<int>

--- lib/crypto/crypto.binding.dart
+++ lib/crypto/crypto.binding.dart
@@ -9,32 +9,89 @@ class CryptoClassBinding extends HTExternalClass {
   dynamic memberGet(String varName, {String? from}) {
     switch (varName) {
       case 'Crypto.sha1':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.sha1(positionalArgs[0]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.sha1(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.sha256':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.sha256(positionalArgs[0]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.sha256(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.sha512':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.sha512(positionalArgs[0]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.sha512(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.md5':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.md5(positionalArgs[0]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) =>
+            Crypto.md5((positionalArgs?[0] as List?)?.cast<int>() as List<int>);
       case 'Crypto.hmacSha1':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.hmacSha1(positionalArgs[0], positionalArgs[1]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.hmacSha1(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+          (positionalArgs?[1] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.hmacSha256':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.hmacSha256(positionalArgs[0], positionalArgs[1]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.hmacSha256(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+          (positionalArgs?[1] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.hmacSha512':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.hmacSha512(positionalArgs[0], positionalArgs[1]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.hmacSha512(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+          (positionalArgs?[1] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.hmacMd5':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.hmacMd5(positionalArgs[0], positionalArgs[1]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.hmacMd5(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+          (positionalArgs?[1] as List?)?.cast<int>() as List<int>,
+        );
       case 'Crypto.hexEncode':
-        return (HTEntity entity, {positionalArgs, namedArgs, typeArgs}) =>
-            Crypto.hexEncode(positionalArgs[0]);
+        return (
+          HTEntity entity, {
+          List? positionalArgs,
+          Map? namedArgs,
+          typeArgs,
+        }) => Crypto.hexEncode(
+          (positionalArgs?[0] as List?)?.cast<int>() as List<int>,
+        );
       default:
         throw HTError.undefined(varName);
     }
